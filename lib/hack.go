d61fd0cb196c7617b5640357bbca23a961b6f80f
Philip Dexter <philip.dexter@gmail.com>
2020-03-23 18:02:13 +0200

    WIP hack
    $ for f in cue.mod/module.cue main.cue okay/okay.cue ; do echo $f ; cat $f ; done
    cue.mod/module.cue
    module: "example.com/blah"
    
    main.cue
    package main
    
    import "example.com/blah/okay"
    
    a: okay.a
    b: "this is b"
    
    okay/okay.cue
    package okay
    
    a: "hi"
    $ mvs hack
    Hack []
    trying to find a
    "hi"
    trying to loop
    - iterator found
    "hi"
    - iterator found
    "this is b"

--- lib/hack.go
+++ lib/hack.go
@@ -2,10 +2,28 @@ package lib
 
 import (
 	"fmt"
+	"os"
+
+	"cuelang.org/go/cue/load"
+	"github.com/hofstadter-io/mvs/lib/util"
 )
 
 func Hack(lang string, args []string) error {
 	fmt.Println("Hack", args)
+
+	bis := load.Instances([]string{}, nil)
+	for _, bi := range bis {
+		if bi.Err != nil {
+			fmt.Println(bi.Err.Error())
+			os.Exit(1)
+		}
+		i, err := util.CueRuntime.Build(bi)
+		if err != nil {
+			fmt.Println(bi.Err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(i.Lookup("a"))
+	}
 
 	return nil
 }
